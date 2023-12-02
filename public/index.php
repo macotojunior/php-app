f9c93b9f230cd4109b33c3eb83aa948213601d0d
Edson Macoto Junior <edson.junior@gazin.com.br>
2023-12-02 13:37:47 -0300

    3 aula

--- public/index.php
+++ public/index.php
@@ -1,14 +1,23 @@
 <?php
+
+use Fig\Http\Message\StatusCodeInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use Integrado\Database;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Psr7\Response as Psr7Response;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
 $app->addErrorMiddleware(true, true, true);
+
+$log = new Logger('php-app');
+$log->pushHandler(new StreamHandler('php://stdout'));
 
 $db = new Database(
     getenv('DATABASE_HOST'),
@@ -17,8 +26,42 @@ $db = new Database(
     getenv('DATABASE_PASS')    
 );
 
+$client = new \Predis\Client('tcp://172.17.0.5:6379', [
+    'parameters' => [
+        'password' => '123456'
+    ]
+]);
+
+$cacheRequest = function (Request $request, RequestHandlerInterface $handler) use ($client, $log) {
+    $cacheName = $request->getUri()->getPath();
+    $cacheValue = $client->get($cacheName);
+
+    if ($cacheValue !== NULL) {
+        $log->info('cache response');
+        $response = new Psr7Response();
+        $response->getBody()->write($cacheValue);
+        return $response->withHeader('Content-Type', 'application/json')
+                        ->withStatus(StatusCodeInterface::STATUS_OK);
+    } else {
+        $log->info('database response');
+        $response = $handler->handle($request);
+        $client->set($cacheName, (string) $response->getBody());
+        $client->expire($cacheName, 10);
+    }
+
+    return $response;
+
+};
+
+$logRequest = function (Request $request, RequestHandlerInterface $handler) use ($log) {
+    $log->info("Início request ".$request->getUri()->getPath());
+    $response = $handler->handle($request);
+    $log->info("Fim request ".$request->getUri()->getPath());
+    return $response;
+};
+
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+    $response->getBody()->write("Olá");
     return $response;
 });
 
@@ -35,6 +78,7 @@ $app->get('/users', function (Request $request, Response $response, array $args)
     return $response
              ->withHeader('Content-Type', 'application/json')
              ->withStatus(200);
-});
+})->add($cacheRequest);
 
+$app->add($logRequest);
 $app->run();
